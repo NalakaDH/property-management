ec57d0503b36ee9e0869e262f2e7eb2fd7443cc9
NalakaDH <nalakadhettiarachchi@gmail.com>
2024-12-10 21:23:12 +0530

    Update

--- src/components/PropertyForm.js
+++ src/components/PropertyForm.js
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { fetchPropertyById, createProperty, updateProperty } from "../services/api";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import L from "leaflet"; // Import Leaflet for marker customization
 import axios from "axios";
 import "leaflet/dist/leaflet.css";
 import "leaflet-geosearch/dist/geosearch.css";
 import "../styles/PropertyForm.css";
+
+// Fix default marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 const PropertyForm = () => {
     const [formData, setFormData] = useState({
